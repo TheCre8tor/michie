dcbbcea10c8d9ce16609226370c81f33a0dabb86
Shahar Dawn Or <mightyiampresence@gmail.com>
2022-06-11 13:54:05 +0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,36 @@
 #![doc = include_str!("../README.md")]
 
 use std::{
+    borrow::Borrow,
     collections::{BTreeMap, HashMap},
-    hash::Hash, borrow::Borrow,
+    hash::Hash,
 };
 
 /// See [crate level documentation](crate).
 pub use michie_macro::memoized;
 
-pub trait MemoizationStore<K, R> {
+pub trait MemoizationStore<K, Q, R>
+where
+    Q: ?Sized,
+{
     fn insert(&mut self, key: K, value: R);
-    fn get<Q>(&self, key: &Q) -> Option<&R>
-        where K: Borrow<Q>,
-              Q: ?Sized;
+    fn get(&self, key: &Q) -> Option<&R>
+    where
+        K: Borrow<Q>;
 }
 
-impl<K, R> MemoizationStore<K, R> for HashMap<K, R>
+impl<K, Q, R> MemoizationStore<K, Q, R> for HashMap<K, R>
 where
     K: Eq + Hash,
+    Q: Hash + Eq,
+    Q: ?Sized,
 {
     fn insert(&mut self, key: K, value: R) {
         HashMap::insert(self, key, value);
     }
-    fn get<Q>(&self, key: &Q) -> Option<&R>
-        where K: Borrow<Q>,
-              Q: ?Sized,
-              Q: Hash + Eq, 
+    fn get(&self, key: &Q) -> Option<&R>
+    where
+        K: Borrow<Q>,
     {
         HashMap::get(self, key)
     }
